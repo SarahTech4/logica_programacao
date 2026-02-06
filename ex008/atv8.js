180d05a5164815538dd2bad07a5ba4cd5648c573
Sarah de Paula <126208084+SarahTech4@users.noreply.github.com>
2026-02-06 20:58:09 -0300

    Primeira atividade com html
    Criação de site com o HTML usando JavaScript para trabalhar conceitos lógicos na programação

---
+++ ex008/atv8.js
@@ -0,0 +1,7 @@
+var num = 5;
+
+console.log('Tabuada do', num)
+
+for (var i = 1; i <=10; i++){
+    console.log(num , "x", i, '=', num * i);
+}
---
+++ ex009/script.js
@@ -0,0 +1,3 @@
+let nome = 'Sarah'
+
+console.log(nome)
